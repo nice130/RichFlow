27fb4b20056fa5da04630fd42c4dd5ad1b377208
minosssss <mh.lee239@gmail.com>
2024-02-12 13:37:47 +0900

    패키지 구조 변경

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+    const key = String(index + 1);
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+        children: new Array(4).fill(null).map((_, j) => {
+            const subKey = index * 4 + j + 1;
+            return {
+                key: subKey,
+                label: `option${subKey}`,
+            };
+        }),
+    };
+});
+const App = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+    return (
+        <Layout>
+            <Header
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <div className="demo-logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={items1}
+                    style={{
+                        flex: 1,
+                        minWidth: 0,
+                    }}
+                />
+            </Header>
+            <Content
+                style={{
+                    padding: '0 48px',
+                }}
+            >
+                <Breadcrumb
+                    style={{
+                        margin: '16px 0',
+                    }}
+                >
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <Layout
+                    style={{
+                        padding: '24px 0',
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Sider
+                        style={{
+                            background: colorBgContainer,
+                        }}
+                        width={200}
+                    >
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{
+                                height: '100%',
+                            }}
+                            items={items2}
+                        />
+                    </Sider>
+                    <Content
+                        style={{
+                            padding: '0 24px',
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Ant Design ©{new Date().getFullYear()} Created by Ant UED
+            </Footer>
+        </Layout>
+    );
+};
+export default App;
