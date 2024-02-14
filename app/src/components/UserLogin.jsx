03691c0b5135022e4b2eba896a0c5ec54dc5fd96
hoon <nice130@naver.com>
2024-02-14 20:04:55 +0900

    UserLogin

--- app/src/components/UserLogin.jsx
+++ app/src/components/UserLogin.jsx
@@ -1,5 +1,28 @@
+import React, { useState } from "react";
+
 export default function UserLogin() {
+    const [userId,setUserId] = useState();
+    const [userPass,setUserPass] = useState();
+
+    const Login = () =>{
+        const memberId=document.getElementById('memberId').value;
+        const memberPassword=document.getElementById('memberPassword').value;
+        console.log('로그인'+memberId);
+        console.log('로그인'+memberPassword);
+    }
+    
     return (
-        <div>Login</div>
+        <div>
+            <div>
+                id :
+                <input type="text" id="memberId"/>
+            </div>
+            <div>
+                pass :
+                <input type="password" id="memberPassword" />
+            </div>
+
+            <button onClick={Login}>로그인</button>
+        </div>
     )
 }
