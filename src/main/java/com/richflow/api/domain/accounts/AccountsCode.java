41dcbcec8bcdbec69d4e3f8aeaca2280d72227ed
minhuikim <kmh940@gmail.com>
2024-03-17 23:45:31 +0900

    2024-03-17 사용자 자산등록, 최상위 자산 생성

--- src/main/java/com/richflow/api/domain/accounts/AccountsCode.java
+++ src/main/java/com/richflow/api/domain/accounts/AccountsCode.java
@@ -1,15 +1,22 @@
 package com.richflow.api.domain.accounts;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class AccountsCode {
-    public static final List<String> CASH = List.of(
-            "현금"
-    );
-    public static final List<String> BANK = List.of(
-            "은행"
-    );
-    public static final List<String> CREDIT = List.of(
-            "신용카드"
-    );
+    public static final String CASH = "현금";
+    public static final String BANK = "은행";
+    public static final String CREDIT = "신용카드";
+
+    private static final HashMap<String, String> variables = new HashMap<>();
+
+    static {
+        variables.put("CASH", AccountsCode.CASH);
+        variables.put("BANK", AccountsCode.BANK);
+        variables.put("CREDIT", AccountsCode.CREDIT);
+    }
+
+    public static String get(String type) {
+        return variables.get(type);
+    }
+
 }
--- src/main/java/com/richflow/api/service/accounts/AccountsService.java
+++ src/main/java/com/richflow/api/service/accounts/AccountsService.java
@@ -5,7 +5,9 @@ import com.richflow.api.domain.accounts.Accounts;
 import com.richflow.api.domain.accounts.AccountsCode;
 import com.richflow.api.domain.enumType.MoneyType;
 import com.richflow.api.repository.accounts.AccountsRepository;
+import com.richflow.api.request.accounts.AccountsRequest;
 import com.richflow.api.request.user.UserRequest;
+import com.richflow.api.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,30 +22,51 @@ import static com.richflow.api.domain.enumType.MoneyType.*;
 public class AccountsService {
 
     private final AccountsRepository accountsRepository;
+    private final UserService userService;
 
-    public void saveBasicAccounts(UserRequest userRequest) {
-        this.saveBasicAccountsOfType(userRequest, cash, AccountsCode.CASH);
-        this.saveBasicAccountsOfType(userRequest, bank, AccountsCode.BANK);
-        this.saveBasicAccountsOfType(userRequest, credit, AccountsCode.CREDIT);
-    }
+    /*
+    * 자산 등록
+    * */
+    public void createAccounts(UserRequest userRequest, AccountsRequest accountsRequest) {
+        Long userIdx = userService.getUserIdxByUserId(userRequest.getUserId());
+        userRequest.setUserIdx(userIdx);
 
-    public void saveBasicAccountsOfType(UserRequest userRequest, MoneyType moneyType, List<String> accountsList) {
-        int seq = 1;
-        for (String accountName : accountsList) {
-            Accounts accounts = buildAndSaveBasicAccount(userRequest, moneyType, accountName, seq++);
-            accountsRepository.save(accounts);
+        MoneyType moneyType = MoneyType.valueOf(accountsRequest.getAcMoneyType().toUpperCase());
+
+        // 사용자 입력 자산 최상위 레벨 확인 및 생성
+        if(!getExistsByAccountsTopLevel(userIdx, moneyType)) {
+            makeAccountsTopLevel(userIdx, moneyType);
         }
+
+        // 사용자 자산 목록 생성
+        Accounts accounts = new Accounts();
+        accounts.setUserIdx(userIdx);
+        accounts.setAcLevel(Math.toIntExact(accountsRequest.getAcLevel()));
+        accounts.setAcMoneyType(valueOf(accountsRequest.getAcMoneyType()));
+        accounts.setAcName(accountsRequest.getAcName());
+        accounts.setAcSeq(accountsRequest.getAcSeq());
+        accounts.setAcCreateAt(CommonUtil.getTimestamp());
+        accountsRepository.save(accounts);
     }
 
-    public Accounts buildAndSaveBasicAccount(UserRequest userRequest, MoneyType moneyType, String accountName, int seq) {
+    /* 
+    * 최상위 레벨 자산 목록 생성
+    * */
+    public void makeAccountsTopLevel(Long userIdx, MoneyType moneyType) {
         Accounts accounts = new Accounts();
-        accounts.setUserIdx(userRequest.getUserIdx());
+        accounts.setUserIdx(userIdx);
         accounts.setAcLevel(1);
         accounts.setAcMoneyType(moneyType);
-        accounts.setAcName(accountName);
-        accounts.setAcSeq(seq);
+        accounts.setAcName(AccountsCode.get(String.valueOf(moneyType)));
         accounts.setAcCreateAt(CommonUtil.getTimestamp());
-        return accounts;
+        accountsRepository.save(accounts);
+    }
+
+    /*
+     * 최상위 레벨 자산 확인
+     * */
+    public Boolean getExistsByAccountsTopLevel(Long userIdx, MoneyType acMoneyType) {
+        return accountsRepository.ExistsByUserIdxAndAcLevelAndAcMoneyType(userIdx, acMoneyType, 1);
     }
 
 }
